95922d29903df100843259e1586e62d5f9100f8c
Matvej Prytkov <matvej.prytkov.02@bk.ru>
2024-07-22 17:34:32 +0500

    commit

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+def home(request):
+    text = """<h1>"Изучаем django"</h1>
+<strong>Автор</strong>: <i>Прытков М. П.</i>
+"""
+    return HttpResponse (text)
 # Create your views here.
