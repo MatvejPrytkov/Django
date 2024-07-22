6366a0d41fde048d84665a3f446151851c9500fb
Matvej Prytkov <matvej.prytkov.02@bk.ru>
2024-07-22 18:03:51 +0500

    2

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -18,5 +18,6 @@ from MainApp import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home), 
+    path('/about', views.dosie),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -5,4 +5,13 @@ def home(request):
 <strong>Автор</strong>: <i>Прытков М. П.</i>
 """
     return HttpResponse (text)
+def dosie(request):
+    name = 'Matvej',
+    surname = 'Prytkov',
+    otch = 'Pavlovich',
+    phone = '+79221223171'
+    email = 'prytkovmatvej@bk.ru'
+    text = {'Имя': name, 'Отчество': otch, 'Фамилия': surname, 'Телефон': phone, 'Почта': email}
+    return HttpResponse (text)
+
 # Create your views here.
