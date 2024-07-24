4bfc2b2bfb46653fee23a651dffe79a87bca57f0
Matvej Prytkov <matvej.prytkov.02@bk.ru>
2024-07-24 19:30:38 +0500

    second

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -5,6 +5,6 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.index_page, name='home'),
-    path('snippets/add', views.add_snippet_page, name='Add_snippets'),
-    path('snippets/list', views.snippets_page, name='Look_snippets'),
+    path('snippets/add', views.add_snippet_page, name='add_snippets'),
+    path('snippets/list', views.snippets_page, name='look_snippets'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
